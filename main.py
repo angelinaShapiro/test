1802e4b5046e9437dbb025f69d21eb7cf3000aa9
Gelya <angelina15v@mail.ru>
2025-06-02 16:16:11 +0300

    next level schedule 7 days
    py code
    you can fill 7 days and change what u need

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
-# Получаем количество дел от пользователя
-n = int(input("Сколько дел у вас запланировано на сегодня? "))
+# Инициализация ежедневника (двумерный массив)
+todo_list = [
+    ['', '', ''],  # Понедельник (утро, день, вечер)
+    ['', '', ''],  # Вторник
+    ['', '', ''],  # Среда
+    ['', '', ''],  # Четверг
+    ['', '', ''],  # Пятница
+    ['', '', ''],  # Суббота
+    ['', '', '']   # Воскресенье
+]
 
-# Создаем пустой список для хранения дел
-todo = []
+# Дни недели для удобства пользователя
+days_of_week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+time_of_day = ["Утро", "День", "Вечер"]
 
-# Запрашиваем дела у пользователя и добавляем их в список
-for i in range(n):
-    task = input(f"Введите дело номер {i + 1}: ")
-    todo.append(task)
+# Заполнение ежедневника делами от пользователя
+print("Заполните свой ежедневник:")
+for day_index, day in enumerate(days_of_week):
+    print(f"\n{day}:")
+    for time_index, time in enumerate(time_of_day):
+        task = input(f"  Введите дело на {time}: ")
+        todo_list[day_index][time_index] = task
 
-# Выводим список дел на экран
-print("\nВаши дела на сегодня:")
-for i, task in enumerate(todo):
-    print(f"{i + 1}. {task}")
+# Удаление записи
+try:
+    delete_day_index = int(input("\nВведите номер дня недели (1-7), из которого хотите удалить дело: ")) - 1
+    delete_time_index = int(input("Введите номер времени суток (1-3), из которого хотите удалить дело (утро, день, вечер): ")) - 1
+    if 0 <= delete_day_index < 7 and 0 <= delete_time_index < 3:
+        deleted_task = todo_list[delete_day_index][delete_time_index]
+        todo_list[delete_day_index][delete_time_index] = ""  # Удаление: просто заменяем на пустую строку
+        print(f"Дело '{deleted_task}' удалено.")
+    else:
+        print("Неверный ввод для удаления.")
+except ValueError:
+    print("Ошибка: Введите корректные номера дня и времени суток.")
 
-# Редактирование дела
-if todo:  # Проверяем, что список не пустой
-    try:
-        edit_index = int(input("\nВведите номер дела, которое хотите отредактировать: ")) - 1  # Вычитаем 1, так как нумерация с 1, а индексы с 0
-        if 0 <= edit_index < len(todo):  # Проверяем корректность введенного номера
-            new_task = input("Введите новое описание дела: ")
-            todo[edit_index] = new_task
-            print("\nСписок дел после редактирования:")
-            for i, task in enumerate(todo):
-                print(f"{i + 1}. {task}")
-        else:
-            print("Неверный номер дела.")
-    except ValueError:
-        print("Ошибка: Введите корректный номер дела.")
+# Добавление записи (редактирование существующей или добавление новой)
+try:
+    add_day_index = int(input("\nВведите номер дня недели (1-7), в который хотите добавить/отредактировать дело: ")) - 1
+    add_time_index = int(input("Введите номер времени суток (1-3), в которое хотите добавить/отредактировать дело (утро, день, вечер): ")) - 1
+    if 0 <= add_day_index < 7 and 0 <= add_time_index < 3:
+        new_task = input("Введите новое дело: ")
+        todo_list[add_day_index][add_time_index] = new_task
+    else:
+        print("Неверный ввод для добавления/редактирования.")
+except ValueError:
+    print("Ошибка: Введите корректные номера дня и времени суток.")
 
-# Удаление дела
-if todo:  # Проверяем, что список не пустой
-    try:
-        delete_index = int(input("\nВведите номер дела, которое хотите удалить: ")) - 1  # Вычитаем 1, так как нумерация с 1, а индексы с 0
-        if 0 <= delete_index < len(todo):  # Проверяем корректность введенного номера
-            deleted_task = todo.pop(delete_index)
-            print(f"\nДело '{deleted_task}' удалено.")
-            print("\nСписок дел после удаления:")
-            for i, task in enumerate(todo):
-                print(f"{i + 1}. {task}")
-        else:
-            print("Неверный номер дела.")
-    except ValueError:
-        print("Ошибка: Введите корректный номер дела.")
+# Вывод ежедневника
+print("\nВаш ежедневник:")
+for day_index, day in enumerate(days_of_week):
+    print(f"\n{day}:")
+    for time_index, time in enumerate(time_of_day):
+        print(f"  {time}: {todo_list[day_index][time_index]}")
